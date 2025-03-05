af6f8d9f3fb1160c504524d4e182a3ed15423e53
Criistiiansiito <crigar34@ucm.es>
2025-03-05 17:46:53 +0100

    Correccion de vistas

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const app = express();
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.set('view engine', 'ejs');  // Configura EJS como motor de plantillas
-app.set('views', path.join(__dirname, 'views'));  // Directorio donde estarán las vistas
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// Configura EJS como motor de plantillas
+app.set('view engine', 'ejs');  
+app.set('views', path.join(__dirname, 'views'));  // Configura la carpeta 'views' donde estarán las plantillas
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -13,10 +18,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+// Rutas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+// Agregar el código para que la aplicación escuche en el puerto
+var port = process.env.PORT || 3000;  // Usar el puerto proporcionado por el entorno o 3000
+app.listen(port, function () {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
 module.exports = app;
+
